65875e64864ce87e8497220538725fa5d5c4d566
Neha <67367474+nehar53@users.noreply.github.com>
2020-09-14 01:42:19 +0700

    Update NavigationBar.dart

--- lib/screens/NavigationBar.dart
+++ lib/screens/NavigationBar.dart
@@ -1,97 +1,105 @@
-import 'dart:convert';
+
 
 import 'package:flutter/material.dart';
-import 'package:flutter_onboarding_ui/screens/HomeScreen/Homepage.dart';
-import 'package:flutter_onboarding_ui/screens/booking.dart';
+import 'package:flutter_app/screens/HomeScreen/Homepage.dart';
+import 'package:flutter_app/screens/booking.dart';
+import 'package:flutter_app/screens/profilescreen.dart';
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 
-import 'package:flutter_onboarding_ui/screens/profilescreen.dart';
+
+
 
 class Navigation extends StatefulWidget {
+  Navigation ({this.phoneNumber1});
+
+  final String phoneNumber1;
   @override
-  _Navigation_Screen createState() => _Navigation_Screen();
+  _MyHomePageState createState() => _MyHomePageState(phoneNumber1:phoneNumber1);
 }
 
-class _Navigation_Screen extends State<Navigation> {
-  int currentTabIndex = 0;
+class _MyHomePageState extends State<Navigation> {
+  _MyHomePageState ({this.phoneNumber1});
+
+  final String phoneNumber1;
+  int currentIndex=0;
   List<Widget> tabs = [
     MyHomePage(),
     BookingScreen(),
     MyHomePage(),
-    MyHomePage(),
-    Profilescreen()
   ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    currentIndex = 0;
+  }
 
-  onTapped(int index) {
+  void changePage(int index) {
     setState(() {
-      currentTabIndex = index;
+      currentIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: tabs[currentTabIndex]),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTapped,
-        currentIndex: currentTabIndex,
-        selectedItemColor: Colors.black,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: Colors.black,
-              size: 30,
-            ),
-            title: Text(
-              "Home",
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.bookmark,
-              color: Colors.black,
-              size: 30,
-            ),
-            title: Text(
-              "Bookings",
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.favorite,
-              color: Colors.black,
-              size: 30,
-            ),
-            title: Text(
-              "UC Safe",
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.account_balance_wallet,
-              color: Colors.black,
-              size: 30,
-            ),
-            title: Text(
-              "Rewards",
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person,
-              color: Colors.black,
-              size: 30,
-            ),
-            title: Text(
-              "Profile",
-              style: TextStyle(color: Colors.black),
-            ),
-          )
+      body: Center(
+        child: tabs[currentIndex],
+      ),floatingActionButton: FloatingActionButton(
+    onPressed: () {Navigator.push(
+    context,
+    MaterialPageRoute(
+    builder: (context) => Profilescreen(phoneNumber3:phoneNumber1)));},
+    child: Icon(Icons.account_box),
+    backgroundColor: Colors.greenAccent,
+    ),
+    floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+
+      bottomNavigationBar: BubbleBottomBar(
+        hasNotch: true,
+        fabLocation: BubbleBottomBarFabLocation.end,
+        opacity: .2,
+        currentIndex: currentIndex,
+        onTap: changePage,
+        borderRadius: BorderRadius.vertical(
+            top: Radius.circular(
+                16)), //border radius doesn't work when the notch is enabled.
+        elevation: 8,
+        items: <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+              backgroundColor: Colors.green,
+              icon: Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.home,
+                color: Colors.green,
+              ),
+              title: Text("Home")),
+
+          BubbleBottomBarItem(
+              backgroundColor: Colors.indigo,
+              icon: Icon(
+                Icons.dashboard,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.dashboard,
+                color: Colors.indigo,
+              ),
+              title: Text("Bookings")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.green,
+              icon: Icon(
+                Icons.account_balance_wallet,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.account_balance_wallet,
+                color: Colors.green,
+              ),
+              title: Text("Rewards"))
         ],
       ),
     );
